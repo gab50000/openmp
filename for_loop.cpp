2e6f142bdf0f047706794ef300a8152378984856
gk <gabriel.kabbe@mail.de>
2014-09-21 11:35:13 +0200

    added sensible parallelization to for_loop.cpp

--- for_loop.cpp
+++ for_loop.cpp
@@ -6,14 +6,30 @@ using namespace std;
 int main(int argc, char** argv){
 
 	int sum = 0;
+	int i;
+	clock_t start = clock();
+	int loop_size = 1000000;
 
+//How not to do it. the atomic statement serializes the whole parallel region
 	#pragma omp parallel for
-	for (int i=0; i<100; i++){
+	for (i=0; i<loop_size; i++){
 		#pragma omp atomic
 		sum += i;
 	}
 
-	cout << sum << endl;
+	cout << "Sum: " << sum << endl;
+	cout << "Needed " << (clock() -start)/ (double) CLOCKS_PER_SEC * 1000 << " msec" << endl;
+
+	sum = 0;
+	start = clock();
+//Here, each thread calculates the sum of its for-loop part, and afterwards the threads' sums are reduced
+	#pragma omp parallel for reduction(+:sum)
+	for (i=0; i<loop_size; i++){
+		sum += i;
+	}
+
+	cout << "Sum: " << sum << endl;
+	cout << "Needed " << (clock() -start)/ (double) CLOCKS_PER_SEC * 1000 << " msec" << endl;
 
 	return 0;
 }
